77847fb5bc5dcd3919c4c751be5ce37df1b47139
Holguin <holguin.pyweb@gmail.com>
2025-06-25 20:53:55 -0500

    subiendo a pythonanyware

--- ziara/settings.py
+++ ziara/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ly^+1un0izuf6&znxq3f^mllu&=o*3kvm5@pl#g37n)+&14#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ziara-render.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
