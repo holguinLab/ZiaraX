2cb0bf76e434baf8e880218c6e64b441a0e34759
Holguin <holguin.pyweb@gmail.com>
2025-07-02 23:01:54 -0500

    tarea de backup automatica de BD

--- ziara/settings.py
+++ ziara/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ly^+1un0izuf6&znxq3f^mllu&=o*3kvm5@pl#g37n)+&14#j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
